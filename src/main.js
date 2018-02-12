6fb002a1b6f2cb5a535821c4f2f997b2c4e377e8
Fabrizio Bertocci <fabrizio@rti.com>
2018-02-12 16:55:24 -0500

    Cleaned up code, added command-line parsing to accept port number from the command line only.

--- src/main.js
+++ src/main.js
@@ -1,42 +1,82 @@
+#!/usr/bin/env node
+/*
+ * (c) 2005-2018  Copyright, Real-Time Innovations, Inc. All rights reserved.
+ * Subject to Eclipse Public License v1.0; see LICENSE.md for details.
+ */
 
-var simplechain = require('./simplechain');
-var p2p = require('./p2p');
-var P2P = p2p.P2P;
-var Block = simplechain.Block;
-var Blockchain = simplechain.Blockchain;
+// ***************************************************************************
+// External Modules
+// ***************************************************************************
+var mod_simplechain = require('./simplechain');
+var mod_p2p = require('./p2p');
+var mod_bodyParser = require('body-parser');
+var mod_express = require('express');
 
-var bodyParser = require('body-parser');
-var express = require('express');
+// ***************************************************************************
+// Globals & Constants
+// ***************************************************************************
+var Block = mod_simplechain.Block;
+var Blockchain = mod_simplechain.Blockchain;
+var P2P = mod_p2p.P2P;
 
-var httpPort = parseInt(process.env.HTTP_PORT) || 3001;
+const DEFAULT_HTTP_PORT = 3001;
+
+
+
+
+// ***************************************************************************
+// Local Functions
+// ***************************************************************************
+function usage() {
+    console.log("Usage: %s [arguments]", process.argv[1]);
+    console.log("Where arguments are:");
+    console.log("  -h, --help          Show this page");
+    console.log("  -v, --verbose       Increase log verbosity");
+    console.log("  -p, --port <port>   Listen port for HTTP Server [%d]", DEFAULT_HTTP_PORT);
+}
+
+
+// ***************************************************************************
+// Main starts here
+// ***************************************************************************
+var httpPort = DEFAULT_HTTP_PORT;
+var verbose  = false;
+
+for (let i = 2; i < process.argv.length; ++i) {
+    if ((process.argv[i] == "-h") || (process.argv[i] == "--help")) {
+        usage();
+        process.exit(1);
+    }
+    if ((process.argv[i] == "-v") || (process.argv[i] == "--verbose")) {
+        verbose = true;
+        continue;
+    }
+    if ((process.argv[i] == "-p") || (process.argv[i] == "--port")) {
+        if (i+1 >= process.argv.length) {
+            console.log("Error: missing argv for '--port' argument");
+            process.exit(1);
+        }
+        httpPort = Number.parseInt(process.argv[++i]);
+        continue;
+    }
+    console.log("Error: unknown argument '%s'", process.argv[i]);
+    process.exit(1);
+}
 
 var blockchain = new Blockchain();
 var p2p = new P2P(blockchain);
 
-var initHttpServer = function (myHttpPort) {
-    var app = express();
-    app.use(bodyParser.json());
-    app.get('/blocks', function (req, res) {
-        res.send(blockchain.getBlockchain());
-    });
-    app.post('/mineBlock', function (req, res) {
-        var newBlock = blockchain.generateNextBlock(req.body.data);
-        res.send(newBlock);
-    });
-    app.listen(myHttpPort, function () {
-        console.log('Listening http on port: ' + myHttpPort);
-    });
-};
+// Initialize express
+var app = mod_express();
+app.use(mod_bodyParser.json());
+app.get('/blocks', function (req, res) {
+    res.send(blockchain.getBlockchain());
+});
+app.post('/mineBlock', function (req, res) {
+    var newBlock = blockchain.generateNextBlock(req.body.data);
+    res.send(newBlock);
+});
+app.listen(httpPort, function () {
+    if (verbose) console.log('Server started, listening http on port: ' + httpPort);
+});
 
-initHttpServer(httpPort);
-
-var test = function() {
-
-    var blockchain = new Blockchain();
-    var p2p = new P2P(blockchain);
-    console.log("Before adding new block:\n");
-    blockchain.print();
-    var newBlock = blockchain.generateNextBlock("I am a new block");
-    console.log("After adding new block:\n");
-    blockchain.print();
-}
